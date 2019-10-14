7c065b1bb69aa278d32abbbe51cffcd7956e2202
Fabio Fernandes <ffernandes@spartaglobal.com>
2019-10-14 11:21:48 +0100

    Make Uppercase Java

---
+++ src/_Codewars/MakeUpperCase.java
@@ -0,0 +1,11 @@
+package _Codewars;
+
+public class MakeUpperCase {
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MakeUpperCase("hello"));
+    }
+}
